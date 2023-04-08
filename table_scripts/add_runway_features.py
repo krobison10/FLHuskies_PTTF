0d407702971124934dbf9829e4ee18186abe481f
Daniil <94884910+Filienko@users.noreply.github.com>
2023-04-08 12:09:20 -0700

    WIP: attempting to make the script run on the VM

--- table_scripts/add_runway_features.py
+++ table_scripts/add_runway_features.py
@@ -26,6 +26,16 @@ def add_runway_features(_df: pd.DataFrame,raw_data:pd.DataFrame, airport:str) ->
     # Filter airport of interest
     current = raw_data.sort_values("timestamp").copy()
 
+    # Add fictitious row in a future timestamp
+    enlarge_configs = pd.DataFrame(
+        {
+            "timestamp": [_df["timestamp"].max()],
+            "airport_config": [raw_data["timestamp"].max()],
+        }
+    )
+    
+    current = current.append(enlarge_configs)
+
     # Aggregate at a 15minute time window
     current = current.groupby("timestamp").airport_config.last().reset_index()
     current = (
