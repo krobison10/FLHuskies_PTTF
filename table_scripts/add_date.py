0db77433de72425eba191cf6cce50f0ec6dbd289
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2023-04-09 13:57:01 -0700

    add some additional features

--- table_scripts/add_date.py
+++ table_scripts/add_date.py
@@ -9,10 +9,11 @@ import pandas as pd  # type: ignore
 
 
 def add_date_features(_df: pd.DataFrame) -> pd.DataFrame:
+    _df["year"] = _df.apply(lambda x: x.timestamp.year, axis=1)
     _df["month"] = _df.apply(lambda x: x.timestamp.month, axis=1)
     _df["day"] = _df.apply(lambda x: x.timestamp.day, axis=1)
     _df["hour"] = _df.apply(lambda x: x.timestamp.hour, axis=1)
-    _df["year"] = _df.apply(lambda x: x.timestamp.year, axis=1)
+    _df["minute"] = _df.apply(lambda x: x.timestamp.minute, axis=1)
     _df["weekday"] = _df.apply(lambda x: x.timestamp.weekday(), axis=1)
 
     # check if the timestamp given is a holiday
