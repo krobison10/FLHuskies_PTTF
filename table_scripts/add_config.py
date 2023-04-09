8b8de3f2399b67814ce7ec2fb10a411fa58e62b7
Daniil <94884910+Filienko@users.noreply.github.com>
2023-04-08 23:43:17 -0700

    Tried to add the capability of listing busyness

--- table_scripts/add_config.py
+++ table_scripts/add_config.py
@@ -1,17 +1,29 @@
 #
-# Author: Jeff Maloney, Kyler Robison
+# Author: Jeff Maloney, Kyler Robison, Daniil Filienko
 #
 # calculate and add airport runway configurations to the data frame
 #
 
 import feature_engineering
 import pandas as pd  # type: ignore
-
+import numpy as np
 
 # find and add current runway configuration
 def add_config(now: pd.Timestamp, flights_selected: pd.DataFrame, data_tables: dict[str, pd.DataFrame]) -> pd.DataFrame:
     # filter features to 30 hours before prediction time to prediction time and save as a copy
     config: pd.DataFrame = feature_engineering.filter_by_timestamp(data_tables["config"], now, 30)
+  
+    # Find the maximum count across all columns
+    max_count_dep = (config["departure_runways"].str.count(',').max()) + 1
+
+    # Find the maximum count across all columns
+    max_count_arr = (config["arrival_runways"].str.count(',').max()) + 1
+
+    # Find the mean dep runways count across all columns
+    mean_count_dep = config['departure_runways'].str.count(',').mean()
+
+    # Find the mean arr runways  count across all columns
+    mean_count_arr = config['arrival_runways'].str.count(',').mean()
 
     # most recent row of airport configuration
     # config_string = config.head(1)['departure_runways'].to_string(index=False)
@@ -24,5 +36,17 @@ def add_config(now: pd.Timestamp, flights_selected: pd.DataFrame, data_tables: d
     flights_selected["departure_runways"] = pd.Series(
         [config_string] * len(flights_selected), index=flights_selected.index
     )
+
+    # Get the ration of how much of the departure runways are used
+    flights_selected['dep_ratio'] = ((config["departure_runways"].str.count(',') + 1) / max_count_dep)
+    
+    # Replacing non existent values with the average
+    flights_selected['dep_ratio'] = np.where(flights_selected['dep_ratio'].isna(), mean_count_dep/max_count_dep, flights_selected['dep_ratio'])
+
+    # Get the ration of how much of the arrival runways are used
+    flights_selected['arr_ratio'] = ((config["arrival_runways"].str.count(',') + 1) / max_count_arr)
+
+    # Replacing non existent values with the average
+    flights_selected['arr_ratio'] = np.where(flights_selected['arr_ratio'].isna(), mean_count_arr/max_count_arr, flights_selected['arr_ratio'])
 
     return flights_selected
--- table_scripts/table_generation.py
+++ table_scripts/table_generation.py
@@ -76,15 +76,15 @@ def generate_table(_airport: str, data_dir: str, max_rows: int = -1) -> pd.DataF
         ),
     }
 
-    # # process all prediction times in parallel
-    # with multiprocessing.Pool() as executor:
-    #     fn = partial(_process_timestamp, flights=_df, data_tables=feature_tables)
-    #     unique_timestamp = _df.timestamp.unique()
-    #     inputs = zip(pd.to_datetime(unique_timestamp))
-    #     timestamp_tables: list[pd.DataFrame] = executor.starmap(fn, tqdm(inputs, total=len(unique_timestamp)))
+    # process all prediction times in parallel
+    with multiprocessing.Pool() as executor:
+        fn = partial(_process_timestamp, flights=_df, data_tables=feature_tables)
+        unique_timestamp = _df.timestamp.unique()
+        inputs = zip(pd.to_datetime(unique_timestamp))
+        timestamp_tables: list[pd.DataFrame] = executor.starmap(fn, tqdm(inputs, total=len(unique_timestamp)))
 
-    # # concatenate individual prediction times to a single dataframe
-    # _df = pd.concat(timestamp_tables, ignore_index=True)
+    # concatenate individual prediction times to a single dataframe
+    _df = pd.concat(timestamp_tables, ignore_index=True)
 
     # # Add runway information
     # _df = _df.merge(feature_tables["runways"][["gufi", "departure_runway_actual"]], how="left", on="gufi")
