a7149b95ddac2eed4d602de58bfb27e73af437a9
krobison10 <kylerrobison10@outlook.com>
2023-04-15 01:38:50 -0700

    Performance optimization. Correctness improvements.

--- table_scripts/add_traffic.py
+++ table_scripts/add_traffic.py
@@ -13,22 +13,27 @@ def add_traffic(
     now: pd.Timestamp, flights_selected: pd.DataFrame, data_tables: dict[str, pd.DataFrame]
 ) -> pd.DataFrame:
     mfs = data_tables["mfs"]
-    etd = data_tables["etd"]
+    latest_etd = data_tables["etd"].groupby("gufi", as_index=False).last()
     runways = data_tables["runways"]
     standtimes = data_tables["standtimes"]
 
-    deps_3hr = count_actual_flights(runways, now, 3, departures=True)
+    runways_filtered_3hr = feature_engineering.filter_by_timestamp(runways, now, 3)
+    runways_filtered_30hr = feature_engineering.filter_by_timestamp(runways, now, 30)
+
+    deps_3hr = count_actual_flights(runways_filtered_3hr, departures=True)
     flights_selected["deps_3hr"] = pd.Series([deps_3hr] * len(flights_selected), index=flights_selected.index)
 
-    deps_30hr = count_actual_flights(runways, now, 30, departures=True)
+    deps_30hr = count_actual_flights(runways_filtered_30hr, departures=True)
     flights_selected["deps_30hr"] = pd.Series([deps_30hr] * len(flights_selected), index=flights_selected.index)
 
-    arrs_3hr = count_actual_flights(runways, now, 3, departures=False)
+    arrs_3hr = count_actual_flights(runways_filtered_3hr, departures=False)
     flights_selected["arrs_3hr"] = pd.Series([arrs_3hr] * len(flights_selected), index=flights_selected.index)
 
-    arrs_30hr = count_actual_flights(runways, now, 30, departures=False)
+    arrs_30hr = count_actual_flights(runways_filtered_30hr, departures=False)
     flights_selected["arrs_30hr"] = pd.Series([arrs_30hr] * len(flights_selected), index=flights_selected.index)
 
+    # technically is the # of planes whom have arrived at destination airport gate and also departed their origin
+    # airport over 30 hours ago, but who cares, it's an important feature regardless
     deps_taxiing = count_planes_taxiing(mfs, runways, standtimes, flights="departures")
     flights_selected["deps_taxiing"] = pd.Series([deps_taxiing] * len(flights_selected), index=flights_selected.index)
 
@@ -37,42 +42,36 @@ def add_traffic(
 
     # apply count of expected departures within various windows
     flights_selected["exp_deps_15min"] = flights_selected.apply(
-        lambda row: count_expected_departures(row["gufi"], etd, 15), axis=1
+        lambda row: count_expected_departures(row["gufi"], latest_etd, 15), axis=1
     )
 
     flights_selected["exp_deps_30min"] = flights_selected.apply(
-        lambda row: count_expected_departures(row["gufi"], etd, 30), axis=1
+        lambda row: count_expected_departures(row["gufi"], latest_etd, 30), axis=1
     )
-
-    # flights_selected["exp_deps_60min"] = flights_selected.apply(
-    #     lambda row: count_expected_departures(row["gufi"], etd, 60), axis=1
-    # )
 
     return flights_selected
 
 
-def count_actual_flights(runways, now, hours: int, departures: bool) -> int:
-    runways = feature_engineering.filter_by_timestamp(runways, now, hours)
+def count_actual_flights(runways_filtered, departures: bool) -> int:
     if departures:
-        runways = runways.loc[pd.notna(runways["departure_runway_actual_time"])]
+        runways_filtered = runways_filtered.loc[pd.notna(runways_filtered["departure_runway_actual_time"])]
     else:
-        runways = runways.loc[pd.notna(runways["arrival_runway_actual_time"])]
+        runways_filtered = runways_filtered.loc[pd.notna(runways_filtered["arrival_runway_actual_time"])]
 
-    return runways.shape[0]
+    return runways_filtered.shape[0]
 
 
 def count_planes_taxiing(mfs, runways, standtimes, flights: str) -> int:
     mfs = mfs.loc[mfs["isdeparture"] == (flights == "departures")]
 
     if flights == "departures":
-        taxi = pd.merge(mfs, standtimes, on="gufi")
-        taxi = pd.merge(taxi, runways, how="left", on="gufi")
-        taxi = taxi.loc[pd.isna(taxi["departure_runway_actual_time"])]
+        taxi = pd.merge(mfs, standtimes, on="gufi") # inner join will only result in flights with departure stand times
+        taxi = pd.merge(taxi, runways, how="left", on="gufi") # left join leaves blanks for taxiing flights
+        taxi = taxi.loc[pd.isna(taxi["departure_runway_actual_time"])] # select the taxiing flights
     elif flights == "arrivals":
-        taxi = pd.merge(mfs, runways, on="gufi")
-        taxi = taxi.loc[pd.notna(taxi["arrival_runway_actual_time"])]
-        taxi = pd.merge(taxi, standtimes, how="left", on="gufi")
-        taxi = taxi.loc[pd.isna(taxi["arrival_stand_actual_time"])]
+        taxi = runways.loc[pd.notna(runways["arrival_runway_actual_time"])] # arrivals are rows with valid time
+        taxi = pd.merge(taxi, standtimes, how="left", on="gufi") # left merge with standtime
+        taxi = taxi.loc[pd.isna(taxi["arrival_stand_actual_time"])] # empty standtimes mean still taxiing
     else:
         raise RuntimeError("Invalid argument, must specify departures or arrivals")
 
@@ -91,7 +90,3 @@ def count_expected_departures(gufi: str, etd: pd.DataFrame, window: int) -> int:
     ]
 
     return etd_window.shape[0]
-
-
-def count_expected_arrivals(data_tables, etd, window):
-    pass
